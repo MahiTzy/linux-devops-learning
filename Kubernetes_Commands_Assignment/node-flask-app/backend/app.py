4ef73385acdb4ed62b69b771aa686095cba7feda
Mohit Singh <116704676+MahiTzy@users.noreply.github.com>
2026-03-31 23:19:33 +0530

    Add files via upload

---
+++ Kubernetes_Commands_Assignment/node-flask-app/backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = request.json
+    print("Received:", data)
+    return jsonify({"message": "Data received successfully"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
