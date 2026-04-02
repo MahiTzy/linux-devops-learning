f8d4f7792d35a4e5def4d9e42d290bb54384d608
Mohit Singh <116704676+MahiTzy@users.noreply.github.com>
2026-04-03 02:45:49 +0530

    Update app.py

--- Kubernetes_Commands_Assignment/node-flask-app/backend/app.py
+++ Kubernetes_Commands_Assignment/node-flask-app/backend/app.py
@@ -2,12 +2,23 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "Flask backend is running successfully 🚀"
+    })
+
+
 @app.route("/submit", methods=["POST"])
 def submit():
     data = request.json
     print("Received:", data)
-    return jsonify({"message": "Data received sucjhbjhcessfully",
-                   "Name": data.name})
+
+    return jsonify({
+        "message": "Data received successfully",
+        "name": data.get("name")
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
