5722fed660b2f01d845e0d13f3a0cef80f14a9fa
Mohit Singh <116704676+MahiTzy@users.noreply.github.com>
2026-04-03 02:57:28 +0530

    Update app.js

--- Kubernetes_Commands_Assignment/node-flask-app/frontend/app.js
+++ Kubernetes_Commands_Assignment/node-flask-app/frontend/app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.post('/submit', async (req, res) => {
     try {
-        await axios.post('http://15.206.88.187:5000/submit', {
+        const response = await axios.post('http://15.206.88.187:5000/submit', {
             name: req.body.name,
             grade: req.body.grade
         });
